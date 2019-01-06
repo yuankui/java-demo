260a9f15cf8fd1b775cf6c1b928bc9bdb562efc2
yuankui <yuankui@meituan.com>
2019-01-06 12:08:18 +0800

    up

---
+++ src/main/java/com/yuankui/java/test/javademo/JavaDemoApplication.java
@@ -0,0 +1,14 @@
+package com.yuankui.java.test.javademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDemoApplication.class, args);
+	}
+
+}
+
