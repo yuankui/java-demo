2ae39313766e54c1229f99fd947a51ba21ea0ef8
yuankui <yuankui@meituan.com>
2019-01-06 14:19:26 +0800

    add fethcers

--- src/test/java/com/yuankui/java/test/javademo/customaop/FetcherTest.java
+++ src/test/java/com/yuankui/java/test/javademo/customaop/FetcherTest.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Import(Config.class)
@@ -20,5 +22,15 @@ public class FetcherTest {
         Object ret = fetcher1.fetch("hello");
 
         System.out.println("ret = " + ret);
+    }
+    
+    @Autowired
+    private List<Fetcher> fetchers;
+    
+    @Test
+    public void fetchers() {
+        for (Fetcher fetcher : fetchers) {
+            System.out.println("fetcher.fetch(\"param\") = " + fetcher.fetch("param"));
+        }
     }
 }
