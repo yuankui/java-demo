7fe4f6ab4bad45bb3f63b4c97fe487e73ab5df97
yuankui <yuankui@meituan.com>
2019-01-06 17:10:27 +0800

    add applicationcontextaware

--- src/main/java/com/yuankui/java/test/javademo/beanfactorypost/BeanDefinitionRegistryPostProcessor1.java
+++ src/main/java/com/yuankui/java/test/javademo/beanfactorypost/BeanDefinitionRegistryPostProcessor1.java
@@ -5,10 +5,12 @@ import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 import org.springframework.beans.factory.support.RootBeanDefinition;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class BeanDefinitionRegistryPostProcessor1 implements BeanDefinitionRegistryPostProcessor {
+public class BeanDefinitionRegistryPostProcessor1 implements BeanDefinitionRegistryPostProcessor, ApplicationContextAware {
     public BeanDefinitionRegistryPostProcessor1() {
         System.out.println("construct");
     }
@@ -20,5 +22,10 @@ public class BeanDefinitionRegistryPostProcessor1 implements BeanDefinitionRegis
     @Override
     public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
         registry.registerBeanDefinition("beanFactoryPostProcessor2", new RootBeanDefinition(BeanFactoryPostProcessor1.class));
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("applicationContext = " + applicationContext);
     }
 }
