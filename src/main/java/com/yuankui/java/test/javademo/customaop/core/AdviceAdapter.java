a622335b5bca33323831089e861c559a88ef3051
yuankui <yuankui@meituan.com>
2019-01-06 18:53:49 +0800

    up

--- src/main/java/com/yuankui/java/test/javademo/customaop/core/AdviceAdapter.java
+++ src/main/java/com/yuankui/java/test/javademo/customaop/core/AdviceAdapter.java
@@ -11,6 +11,32 @@ public class AdviceAdapter {
     private final Class clazz;
     private Advice advice;
 
+    private class Wrapper implements InvocationHandler {
+        private Advice advice;
+        private Object object;
+
+        public Wrapper(Advice advice, Object object) {
+            this.advice = advice;
+            this.object = object;
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            // 代理的调用要经过advice对象封装，当前前提是方法名要匹配
+            if (method.equals(AdviceAdapter.this.method)) {
+                return advice.process(() -> {
+                    try {
+                        return method.invoke(object, args);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+            } else {
+                // 方法名不匹配，就直接退出
+                return method.invoke(object, args);
+            }
+        }
+    }
     /**
      * 解析注解中的class，method
      * @param advice
@@ -39,21 +65,7 @@ public class AdviceAdapter {
         return false;
     }
     public Object wrap(Object object) {
-        InvocationHandler handler = (proxy, method, args) -> {
-            // 代理的调用要经过advice对象封装，当前前提是方法名要匹配
-            if (method.equals(this.method)) {
-                return advice.process(() -> {
-                    try {
-                        return method.invoke(object, args);
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                });
-            } else {
-                // 方法名不匹配，就直接退出
-                return method.invoke(object, args);
-            }
-        };
+        InvocationHandler handler = new Wrapper(advice, object);
         return Proxy.newProxyInstance(this.getClass().getClassLoader(), new Class[]{clazz}, handler);
     }
 }
