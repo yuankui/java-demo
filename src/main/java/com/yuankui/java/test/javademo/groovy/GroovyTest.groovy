c5bc8d254565d24b4a80f16b8c85beaa283a7042
yuankui <yuankui@meituan.com>
2019-03-15 17:33:15 +0800

    up

---
+++ src/main/java/com/yuankui/java/test/javademo/groovy/GroovyTest.groovy
@@ -0,0 +1,14 @@
+import com.yuankui.java.test.javademo.groovy.xml.Xml
+
+def xml = new Xml()
+
+xml = xml.children() {
+    child([name: "yuankui1"]) {
+        name([age: 12])
+    }
+    child([name: "yuankui2"])
+    child([name: "yuankui3"])
+}
+
+println(xml.toXml())
+
