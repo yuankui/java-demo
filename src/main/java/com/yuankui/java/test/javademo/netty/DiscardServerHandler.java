baf66ac540eaa75cb9ff31ee3b537f1a5267fa07
yuankui <yuankui@meituan.com>
2019-05-09 20:10:18 +0800

    up

---
+++ src/main/java/com/yuankui/java/test/javademo/netty/DiscardServerHandler.java
@@ -0,0 +1,22 @@
+package com.yuankui.java.test.javademo.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+        // Discard the received data silently.
+        ByteBuf byteBuf = (ByteBuf) msg;
+        byteBuf.release(); // (3)
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
