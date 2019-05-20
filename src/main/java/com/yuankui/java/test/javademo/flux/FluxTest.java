77878dd86a615ffd4aa4b5765c3f99358ca74619
yuankui <yuankui@meituan.com>
2019-05-20 17:46:59 +0800

    replace stream -> flux

--- src/main/java/com/yuankui/java/test/javademo/flux/FluxTest.java
+++ src/main/java/com/yuankui/java/test/javademo/flux/FluxTest.java
@@ -1,11 +1,26 @@
 package com.yuankui.java.test.javademo.flux;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.SynchronousSink;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.BiConsumer;
 
 public class FluxTest {
     public static void main(String[] args) {
+        List<String> strings = Arrays.asList("hello", "kitty");
+
+        Integer max = Flux.fromIterable(strings)
+                .map(String::length)
+                .reduce(Math::max)
+                .block();
+
+        System.out.println("max = " + max);
+    }
+
+    private static void test1() {
         Flux<Byte> bytes = Flux.fromStream("1234567890".chars().boxed())
                 .map(Integer::byteValue);
 
@@ -14,10 +29,25 @@ public class FluxTest {
                 .map(FluxTest::mergeBytesToInt)
                 .subscribe(list -> System.out.println("int = " + list));
     }
-    
+
     private static int mergeBytesToInt(List<Byte> bytes) {
         return bytes.stream()
                 .mapToInt(Byte::intValue)
                 .reduce(0, (a, b) -> a * 256 + b);
     }
+
+    private static <S> Flux<List<S>> handle(Flux<S> flux) {
+        int size = 3;
+        List<S> buf = new ArrayList<>();
+        BiConsumer<S, SynchronousSink<List<S>>> buffer = (s, r) -> {
+            buf.add(s);
+            if (buf.size() >= size) {
+                r.next(buf);
+                buf.clear();
+            }
+        };
+
+        return flux.handle(buffer);
+    }
+
 }
