0977e59522f5c9d74791e29e0ea54a8e87b5fc50
yuankui <yuankui@meituan.com>
2019-05-23 10:43:30 +0800

    up

--- src/main/java/com/yuankui/java/test/javademo/rxjava/RxJavaTest.java
+++ src/main/java/com/yuankui/java/test/javademo/rxjava/RxJavaTest.java
@@ -2,6 +2,8 @@ package com.yuankui.java.test.javademo.rxjava;
 
 import rx.Observable;
 
+import java.util.List;
+
 public class RxJavaTest {
     public static void main(String[] args) {
         Observable.range(1, 100)
@@ -12,5 +14,18 @@ public class RxJavaTest {
                 .toSingle()
                 .toBlocking()
                 .value();
+
+        String str;
+
     }
+
+    private static String doIt(List<Integer> list) {
+        return list.stream()
+                .filter(i -> (i % 2 == 0))
+                .map(Object::toString)
+                .reduce((left, right) -> left + "; " + right)
+                .map(csv -> "[" + csv + "]")
+                .orElse("-");
+    }
+
 }
