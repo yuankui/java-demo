753cf759eb6228f75692e5247badebdab8a36ab3
yuankui <yuankui@meituan.com>
2020-01-07 20:24:36 +0800

    up

--- src/main/java/com/yuankui/java/test/javademo/rxjava/RxJavaTest.java
+++ src/main/java/com/yuankui/java/test/javademo/rxjava/RxJavaTest.java
@@ -2,28 +2,18 @@ package com.yuankui.java.test.javademo.rxjava;
 
 import rx.Observable;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class RxJavaTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        Map<String, Integer> map = Observable.from(Arrays.asList("hello", "kitty"))
-                .toMap(k -> k, k -> k.length())
-                .toBlocking()
-                .first();
+        Observable.interval(500, TimeUnit.MILLISECONDS)
+                .buffer(3)
+                .subscribe(list -> {
+                    System.out.println("list = " + list);
+                });
 
-        System.out.println("map = " + map);
+        
+        TimeUnit.SECONDS.sleep(20);
     }
-
-    private static String doIt(List<Integer> list) {
-        return list.stream()
-                .filter(i -> (i % 2 == 0))
-                .map(Object::toString)
-                .reduce((left, right) -> left + "; " + right)
-                .map(csv -> "[" + csv + "]")
-                .orElse("-");
-    }
-
 }
