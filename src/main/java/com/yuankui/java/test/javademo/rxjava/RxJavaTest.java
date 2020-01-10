f6e50394681871f7eb785d19dad5759fa09f24fb
yuankui <yuankui@meituan.com>
2020-01-10 16:13:01 +0800

    flatmap buffer

--- src/main/java/com/yuankui/java/test/javademo/rxjava/RxJavaTest.java
+++ src/main/java/com/yuankui/java/test/javademo/rxjava/RxJavaTest.java
@@ -1,16 +1,31 @@
 package com.yuankui.java.test.javademo.rxjava;
 
-import org.apache.commons.lang3.tuple.Triple;
 import rx.Observable;
+import rx.functions.Func1;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RxJavaTest {
     public static void main(String[] args) throws InterruptedException {
         Observable.range(1, 10)
-                .scan(Integer::sum)
-                .subscribe(integer -> {
-                    System.out.println("integer = " + integer);
-                });
+                .flatMap(new Func1<Integer, Observable<List<Integer>>>() {
+                    private List<Integer> ints = new ArrayList<>();
 
-        Triple<Integer, String, Integer> triple = Triple.of(1, "", 13);
+                    @Override
+                    public Observable<List<Integer>> call(Integer integer) {
+                        ints.add(integer);
+                        if (ints.size() == 3) {
+                            try {
+                                return Observable.just(ints);
+                            } finally {
+                                this.ints = new ArrayList<>();
+                            }
+                        } else {
+                            return Observable.empty();
+                        }
+                    }
+                })
+                .subscribe(i -> System.out.println("i = " + i));
     }
 }
