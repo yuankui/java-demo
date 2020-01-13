6cf58f3b1c6043e23ba4d759d3d79c3f7663b17d
yuankui <yuankui@meituan.com>
2020-01-13 16:06:52 +0800

    AsyncOnSubscribe

--- src/main/java/com/yuankui/java/test/javademo/rxjava/RxJavaTest.java
+++ src/main/java/com/yuankui/java/test/javademo/rxjava/RxJavaTest.java
@@ -1,22 +1,30 @@
 package com.yuankui.java.test.javademo.rxjava;
 
 import rx.Observable;
-
-import java.util.concurrent.TimeUnit;
+import rx.Observer;
+import rx.observables.AsyncOnSubscribe;
 
 public class RxJavaTest {
     public static void main(String[] args) throws InterruptedException {
-        Observable<Long> observable = Observable.interval(1, TimeUnit.SECONDS)
-                .publish()
-                .refCount();
+        Observable.create(new AsyncOnSubscribe<Integer, Integer>() {
+            @Override
+            protected Integer generateState() {
+                return 0;
+            }
 
-
-        observable.subscribe(a -> System.out.println("1 = " + a));
-
-        TimeUnit.SECONDS.sleep(5);
-
-        observable.subscribe(a -> System.out.println("2 = " + a));
-        
-        TimeUnit.SECONDS.sleep(5);
+            @Override
+            protected Integer next(Integer state, long requested, Observer<Observable<? extends Integer>> observer) {
+                Observable<Integer> take = Observable.range(state, Integer.MAX_VALUE)
+                        .take((int) requested);
+                
+                
+                state = state + (int)requested;
+                observer.onNext(take);
+                
+                return state;
+            }
+        })
+                .take(10)
+                .subscribe(a -> System.out.println("a = " + a));
     }
 }
